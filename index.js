7f57c02ac6320c7f05f67a7c3ff92de9551f5255
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-18 00:37:56 +0100

    Using nodemon for dev. Set CORS headers on all routes

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({port: parseInt(process.env.PORT) || 3000});
+server.connection({
+    port: parseInt(process.env.PORT) || 3000,
+    routes: {cors: true}
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        return reply('success');
+    }
 });
