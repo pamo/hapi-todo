4cb0565a0f2af774cd4b96bba4574328c47c9b74
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-18 00:10:14 +0100

    use system port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({port: parseInt(process.env.PORT) || 3000});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
