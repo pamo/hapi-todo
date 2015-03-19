b18af2badc561cdd250d36d43576d758c9cdc452
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-19 23:30:30 +0100

    Delete todos

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ server.register({
     options: dbOpts
 }, function (err) {
     if (err) {
-        console.error(err);
         throw err;
     }
 });
@@ -21,25 +20,7 @@ server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
 
-var getAllTodos = function(request, reply){
-    var db = request.server.plugins['hapi-mongodb'].db;
-    db.collection('todos').find().toArray(function(err, doc){
-        return reply(doc);
-    });
-};
-
-var getTodoById = function(request, reply){
-    var db = request.server.plugins['hapi-mongodb'].db;
-    var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
-    db.collection('todos')
-    .findOne({"_id": new ObjectID(request.params.id)}, function(err, doc){
-        console.log('\t\t\t\t' + doc);
-        return reply(doc);
-    });
-};
-
-
-var saveTodoToDb = function(request, reply){
+var save = function(request, reply){
     var todo = { 
             title: request.payload.title, 
             order: request.payload.order 
@@ -56,21 +37,68 @@ var saveTodoToDb = function(request, reply){
     });
 };
 
+var getAll = function(request, reply){
+    var db = request.server.plugins['hapi-mongodb'].db;
+    db.collection('todos').find().toArray(function(err, doc){
+        return reply(doc);
+    });
+};
+
+var getById = function(request, reply){
+    var db = request.server.plugins['hapi-mongodb'].db;
+    var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+    db.collection('todos')
+    .findOne({"_id": new ObjectID(request.params.id)}, function(err, doc){
+        return reply(doc);
+    });
+};
+
+var deleteAll = function(request, reply) {
+    var db = request.server.plugins['hapi-mongodb'].db;
+
+    db.collection('todos').remove({}, function (err){
+        if (err) return reply(Hapi.error.internal('Internal MongoDB error', err));
+        reply("Deleted all todos");
+    });
+};
+
+var deleteById = function(request, reply) {
+    var db = request.server.plugins['hapi-mongodb'].db;
+    var ObjectID = request.server.plugins['hapi-mongodb'].ObjectID;
+
+    db.collection('todos').remove({"_id": new ObjectID(request.params.id)}, function (err){
+        if (err) return reply(Hapi.error.internal('Internal MongoDB error', err));
+        reply("Record Deleted");
+    });
+};
+
 server.route({
     method: 'GET',
     path: '/',
-    handler: getAllTodos
+    handler: getAll
 });
 
 server.route({
     method: 'POST',
     path: '/',
-    handler: saveTodoToDb
+    handler: save
 });
+
+server.route({
+    method: 'DELETE',
+    path: '/',
+    handler: deleteAll
+})
 
 server.route({
     method: 'GET',
     path: '/{id}',
-    handler: getTodoById
+    handler: getById
 });
+
+server.route({
+    method: 'DELETE',
+    path: '/{id}',
+    handler: deleteById
+})
 
