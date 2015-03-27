75a50e63ddbc514e40df36cba136dd97b69d9328
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-03-27 23:43:54 +0100

    Added heroku mongo addon config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
     mongo: {
-        "url": "mongodb://localhost:27017/todos",
+        "url": process.env.MONGOLAB_URI || "mongodb://localhost:27017/todos",
         "settings": {
             "db": {
                 "native_parser": false
